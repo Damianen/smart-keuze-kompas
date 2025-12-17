755289363cbcba3753f8e0bcfc23e743e6668e7d
EnesT51 <Enes.t51@outlook.com>
2025-12-17 17:16:37 +0100

    Rate_limiting geimplementeert, voorkom van burute-force, abuses en DDoS attacks, puur security gerelateerd.

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,11 +1,37 @@
 import { Module } from '@nestjs/common';
 import { InfrastructureModule } from './infrastructuur/infrastructure.module';
 import { PresentationModule } from './presentation/presentation.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+import { ConfigService, ConfigModule } from '@nestjs/config';
+import { seconds } from '@nestjs/throttler';
+import { AuthGuard } from './infrastructuur/auth/guard/auth.guard';
 
 @Module({
 	imports: [
 		InfrastructureModule, 
 		PresentationModule,
+		ConfigModule.forRoot({
+			isGlobal: true,
+			cache: true,
+			envFilePath: `.env`,
+		}),
+		ThrottlerModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: (config: ConfigService) => [
+				{ 
+					ttl: seconds(config.get<number>('THROTTLE_TTL', 60)),
+				  	limit: config.get<number>('THROTTLE_LIMIT', 100),
+				}
+			]
+		}),
 	],
+	providers: [
+		{
+			provide: APP_GUARD,
+			useClass: ThrottlerGuard,
+		}
+	]
 })
 export class AppModule {}
