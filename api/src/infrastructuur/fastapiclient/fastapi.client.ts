b69d8f4c6a304c0642d9697c6f001508659afcdb
EnesT51 <Enes.t51@outlook.com>
2025-12-19 15:05:15 +0100

    configservice toegevoegd om env variablen te ophalen

--- api/src/infrastructuur/fastapiclient/fastapi.client.ts
+++ api/src/infrastructuur/fastapiclient/fastapi.client.ts
@@ -2,31 +2,32 @@ import { HttpService } from '@nestjs/axios/';
 import { catchError, firstValueFrom, map } from 'rxjs';
 import { Injectable } from '@nestjs/common';
 import { KeuzemoduleAIEntity } from 'src/core/recommender-system/entity/keuzemodule.ai.entity';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class FastApiClient {
 
-    constructor(private readonly httpService: HttpService) {}
+    constructor(private readonly httpService: HttpService, private readonly configService: ConfigService) {}
 
     async getRecommendations(params: any): Promise<KeuzemoduleAIEntity[]> {
         try {
             const heaaderRequest = {
                 'Content-Type': 'application/json',
-                'Accept': 'application/json'
-                // 'Authorization': `api-key` // hier moet ik nog de api-key toevoegen om gebruik te kunnen maken van de fastapi service endpoint.
+                'Accept': 'application/json',
+                'x-api-key': `${this.configService.get('API_KEY')}`
             };
             const {data} = await firstValueFrom(
-                this.httpService.post<KeuzemoduleAIEntity[]>('https://fab0ee7a-03fd-41c0-b58f-8e29c39424fa.mock.pstmn.io/api/recommender-system/recommendations', params, { headers: heaaderRequest }).pipe(
+                this.httpService.post<KeuzemoduleAIEntity[]>(`${this.configService.get('FAST_API_URL')}`, params, { headers: heaaderRequest }).pipe(
                     catchError((error) => {
                             console.error('Error ontstaan bij het ophalen van aanbevelingen:');
-                        throw error;
-                    })
+                            throw error;
+                    }),
                 )
             );
             return data['recommendations'];
     }catch (error) {
             console.error('Error bij ophalen aanbevelingen:', error);
-            throw error;
+            return error;
         }
     }
 }
