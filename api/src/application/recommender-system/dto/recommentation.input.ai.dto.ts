52f7eb297d8125c6c4fc6c8195d3334ef2beb9af
EnesT51 <Enes.t51@outlook.com>
2026-01-14 22:34:55 +0100

    save

--- api/src/application/recommender-system/dto/recommentation.input.ai.dto.ts
+++ api/src/application/recommender-system/dto/recommentation.input.ai.dto.ts
@@ -1,7 +1,9 @@
 import {IsNotEmpty, IsString} from 'class-validator';
-
+import { Transform } from 'class-transformer';
+import { sanitizeInput } from 'src/utils/utils';
 export class RecommendationInputDto {
     @IsNotEmpty()
     @IsString()
+    @Transform(({ value }) => sanitizeInput(value))
     student_text: string;
 }
